cacd20dff1e439176ce4e9962b7ac44f99e9d466
ahmetbcakici <ahmetbcakici@gmail.com>
2020-05-14 03:51:00 +0300

    some changes with recommended options

--- settings.js
+++ settings.js
@@ -1,9 +1,17 @@
 module.exports = {
-  E_MAIL: 'Your linkedin email',
-  PASS: 'Your linkedin password',
+  E_MAIL: 'Your e-mail',
+  PASS: 'Your password',
   EVENT_LINK: 'Your event link',
 
   SCROLL_DISTANCE: 70, // recommended
-  SPEED: 100, // recommended
-  CONNECTION_LIMIT: 100, // How many people you want to select?
+  SPEED: 1000, // recommended
+  CONNECTION_LIMIT: 500, // How many people you want to select?
+  /*
+    WARNING!
+    
+    If your computer is not very powerful please be careful with speed configuration!
+    THere could be a slowdown situation. 
+    Also you should be careful with connection_limit option.
+    Please do not keep this options very high
+  */
 };
